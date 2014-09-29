3273815f5aea89e311266bfb3004a761addcaeb0
Michael Galloy <mgalloy@gmail.com>
2014-09-29 17:42:03 -0600

    Adding log output for Moves App job.

--- moves.sh
+++ moves.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-python moves.py
-scp ~/data/movesapp.json idldev.com:~/data.idldev.com/
+LOG=/Users/mgalloy/data/moves.py.log
+
+/Users/mgalloy/anaconda/python /Users/mgalloy/bin/moves.py
+/usr/bin/scp -i ~mgalloy/.ssh/id_dsa2 ~/data/movesapp.json idldev.com:~/data.idldev.com/ >> $LOG 2>&1
+
+d=`date`
+user=`whoami`
+echo "Performed movesapp.sh as $user at $d" >> $LOG
