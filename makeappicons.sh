a4f47ee419b89f87bbaf31aacaf2c8cc44823505
Michael Galloy <mgalloy@gmail.com>
2015-02-21 15:52:37 -0700

    Making correct sizes for app icons.

--- makeappicons.sh
+++ makeappicons.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
+# give help if no arguments
+if [ $# -ne 2 ]; then
+  echo "syntax: $0 imagefile outputdir"
+  exit 1
+fi
+
 INPUT_IMAGE="$1"
 FOLDER=$2
 
 mkdir -p ${FOLDER}
 
-sips -z 57 57 "$INPUT_IMAGE" --out "${FOLDER}/Icon.png"
-sips -z 114 114 "$INPUT_IMAGE" --out "${FOLDER}/Icon@2x.png"
+sips -z 29 29 "$INPUT_IMAGE" --out "${FOLDER}/Icon-29.png"
+sips -z 58 58 "$INPUT_IMAGE" --out "${FOLDER}/Icon-29@2x.png"
+sips -z 87 87 "$INPUT_IMAGE" --out "${FOLDER}/Icon-29@3x.png"
+
+sips -z 40 40 "$INPUT_IMAGE" --out "${FOLDER}/Icon-40.png"
+sips -z 80 80 "$INPUT_IMAGE" --out "${FOLDER}/Icon-40@2x.png"
+sips -z 120 120 "$INPUT_IMAGE" --out "${FOLDER}/Icon-40@3x.png"
+
+sips -z 60 60 "$INPUT_IMAGE" --out "${FOLDER}/Icon-60.png"
 sips -z 120 120 "$INPUT_IMAGE" --out "${FOLDER}/Icon-60@2x.png"
-sips -z 29 29 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small.png"
-sips -z 58 58 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small@2x.png"
-sips -z 40 40 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small-40.png"
-sips -z 80 80 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small-40@2x.png"
-sips -z 50 50 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small-50.png"
-sips -z 100 100 "$INPUT_IMAGE" --out "${FOLDER}/Icon-Small-50@2x.png"
-sips -z 72 72 "$INPUT_IMAGE" --out "${FOLDER}/Icon-72.png"
-sips -z 144 144 "$INPUT_IMAGE" --out "${FOLDER}/Icon-72@2x.png"
+sips -z 180 180 "$INPUT_IMAGE" --out "${FOLDER}/Icon-60@3x.png"
+
 sips -z 76 76 "$INPUT_IMAGE" --out "${FOLDER}/Icon-76.png"
 sips -z 152 152 "$INPUT_IMAGE" --out "${FOLDER}/Icon-76@2x.png"
