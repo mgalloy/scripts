f2e376e0c2c8b2ab194a007064ba0d9dd072e687
Michael Galloy <mgalloy@gmail.com>
2015-03-02 09:54:43 -0700

    Checking to see if brew and conda are installed before updating, so can be used to update anaconda on Linux systems without brew.

--- update_software.sh
+++ update_software.sh
@@ -4,6 +4,7 @@ DATA_DIR=$HOME/data
 LINE=`printf %80s | tr " " "="`
 INDENT="  "
 
+
 stamp() {
   OUTFILE=$DATA_DIR/$1-$2.log
   echo "\n$LINE" >> $OUTFILE
@@ -12,38 +13,51 @@ stamp() {
   echo "$LINE\n" >> $OUTFILE
 }
 
-stamp conda update
-if yes | conda update conda >> $DATA_DIR/conda-update.log 2>&1; then
-  echo "conda updated"
-  stamp anaconda update
-  if yes | conda update anaconda >> $DATA_DIR/anaconda-update.log 2>&1; then
-    echo "anaconda updated"
-  else
-    echo "Problem updating anaconda"
-  fi
-else
-  echo "Problem updating conda"
+
+if [ ! -d $DATA_DIR ]; then
+  echo "Creating data directory at $DATA_DIR..."
+  mkdir -p $DATA_DIR
 fi
 
 
-stamp brew update
-if brew update >> $DATA_DIR/brew-update.log 2>&1; then
-  echo "homebrew updated"
-  TO_BE_UPGRADED=`brew outdated`
-  if [ ${#TO_BE_UPGRADED} -gt 0 ]; then
-    echo "homebrew formula to be upgraded:"
-    for item in $TO_BE_UPGRADED; do
-      echo "$INDENT$item"
-    done
-    stamp brew upgrade
-    if brew upgrade >> $DATA_DIR/brew-upgrade.log 2>&1; then
-      echo "homebrew formulas upgraded"
+CONDA_FOUND=`which conda 2> /dev/null`
+if [ -n "$CONDA_FOUND" ]; then
+  stamp conda update
+  if yes | conda update conda >> $DATA_DIR/conda-update.log 2>&1; then
+    echo "conda updated"
+    stamp anaconda update
+    if yes | conda update anaconda >> $DATA_DIR/anaconda-update.log 2>&1; then
+      echo "anaconda updated"
     else
-      echo "Problem upgrading homebrew formula"
+      echo "Problem updating anaconda"
     fi
   else
-    echo "no homebrew formulas to upgrade"
+    echo "Problem updating conda"
   fi
-else
-  echo "Problem updating homebrew"
+fi
+
+
+BREW_FOUND=`which brew 2> /dev/null`
+if [ -n "$BREW_FOUND" ]; then
+  stamp brew update
+  if brew update >> $DATA_DIR/brew-update.log 2>&1; then
+    echo "homebrew updated"
+    TO_BE_UPGRADED=`brew outdated`
+    if [ ${#TO_BE_UPGRADED} -gt 0 ]; then
+      echo "homebrew formula to be upgraded:"
+      for item in $TO_BE_UPGRADED; do
+        echo "$INDENT$item"
+      done
+      stamp brew upgrade
+      if brew upgrade >> $DATA_DIR/brew-upgrade.log 2>&1; then
+        echo "homebrew formulas upgraded"
+      else
+        echo "Problem upgrading homebrew formula"
+      fi
+    else
+      echo "no homebrew formulas to upgrade"
+    fi
+  else
+    echo "Problem updating homebrew"
+  fi
 fi
