4a3a137803fc18a747c3f8018933b75537fb454b
Michael Galloy <mgalloy@gmail.com>
2015-04-13 17:35:04 -0600

    Using appropriate options to echo command on various platforms.

--- update_software.sh
+++ update_software.sh
@@ -3,19 +3,26 @@
 DATA_DIR=$HOME/data
 LINE=`printf %80s | tr " " "="`
 INDENT="  "
-
+case `uname` in
+  Linux)
+    ECHO_CMD="echo -e"
+    ;;
+  Darwin)
+    ECHO_CMD="echo"
+    ;;
+esac
 
 stamp() {
   OUTFILE=$DATA_DIR/$1-$2.log
-  echo -e "\n$LINE" >> $OUTFILE
-  echo "Updating $1" >> $OUTFILE
-  echo `date` >> $OUTFILE
-  echo -e "$LINE\n" >> $OUTFILE
+  $ECHO_CMD "\n$LINE" >> $OUTFILE
+  $ECHO_CMD "Updating $1" >> $OUTFILE
+  $ECHO_CMD `date` >> $OUTFILE
+  $ECHO_CMD "$LINE\n" >> $OUTFILE
 }
 
 
 if [ ! -d $DATA_DIR ]; then
-  echo "Creating data directory at $DATA_DIR..."
+  $ECHO_CMD "Creating data directory at $DATA_DIR..."
   mkdir -p $DATA_DIR
 fi
 
@@ -24,16 +31,16 @@ CONDA_FOUND=`which conda 2> /dev/null`
 if [ -n "$CONDA_FOUND" ]; then
   stamp conda update
   if yes | conda update conda >> $DATA_DIR/conda-update.log 2>&1; then
-    echo "conda updated"
+    $ECHO_CMD "conda updated"
     stamp anaconda update
     if yes | conda update anaconda >> $DATA_DIR/anaconda-update.log 2>&1; then
-      echo "anaconda updated"
+      $ECHO_CMD "anaconda updated"
       conda list > $DATA_DIR/conda-list.log 2>&1
     else
-      echo "Problem updating anaconda"
+      $ECHO_CMD "Problem updating anaconda"
     fi
   else
-    echo "Problem updating conda"
+    $ECHO_CMD "Problem updating conda"
   fi
 fi
 
@@ -42,23 +49,23 @@ BREW_FOUND=`which brew 2> /dev/null`
 if [ -n "$BREW_FOUND" ]; then
   stamp brew update
   if brew update >> $DATA_DIR/brew-update.log 2>&1; then
-    echo "homebrew updated"
+    $ECHO_CMD "homebrew updated"
     TO_BE_UPGRADED=`brew outdated`
     if [ ${#TO_BE_UPGRADED} -gt 0 ]; then
-      echo "homebrew formula to be upgraded:"
+      $ECHO_CMD "homebrew formula to be upgraded:"
       for item in $TO_BE_UPGRADED; do
-        echo "$INDENT$item"
+        $ECHO_CMD "$INDENT$item"
       done
       stamp brew upgrade
       if brew upgrade >> $DATA_DIR/brew-upgrade.log 2>&1; then
-        echo "homebrew formulas upgraded"
+        $ECHO_CMD "homebrew formulas upgraded"
       else
-        echo "Problem upgrading homebrew formula"
+        $ECHO_CMD "Problem upgrading homebrew formula"
       fi
     else
-      echo "no homebrew formulas to upgrade"
+      $ECHO_CMD "no homebrew formulas to upgrade"
     fi
   else
-    echo "Problem updating homebrew"
+    $ECHO_CMD "Problem updating homebrew"
   fi
 fi
