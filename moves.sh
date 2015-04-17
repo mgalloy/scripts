67be19603ff1207c3385a59dc4d0bd9a9b8db918
Michael Galloy <mgalloy@gmail.com>
2015-04-16 21:13:24 -0600

    Redirecting errors in performing Moves API calls to Moves log.

--- moves.sh
+++ moves.sh
@@ -2,9 +2,9 @@
 
 LOG=/Users/mgalloy/data/moves.py.log
 
-/Users/mgalloy/anaconda/bin/python /Users/mgalloy/bin/moves.py
+/Users/mgalloy/anaconda/bin/python /Users/mgalloy/bin/moves.py >> $LOG 2>&1
 /usr/bin/scp -i ~mgalloy/.ssh/id_dsa2 ~/data/movesapp.json idldev.com:~/data.idldev.com/ >> $LOG 2>&1
 
 d=`date`
 user=`whoami`
-echo "Performed movesapp.sh as $user at $d" >> $LOG
+echo "Performed moves.sh as $user at $d" >> $LOG
