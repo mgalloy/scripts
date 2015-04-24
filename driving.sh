e36d88d1709ac6355ee608f13e9e64a2d0999664
Michael Galloy <mgalloy@gmail.com>
2015-04-24 17:30:38 -0600

    Adding bash script to run driving.py suitable for adding to crontab.

---
+++ driving.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+INPUT=$HOME/Dropbox/driving/driving.csv
+BIN_DIR=$HOME/bin
+DATA_DIR=$HOME/data
+OUTPUT=$DATA_DIR/weekly-driving.csv
+LOG=$DATA_DIR/driving.log
+SERVER_LOC=idldev.com:~/data.idldev.com/
+
+$BIN_DIR/driving.py $INPUT --output $OUTPUT >> $LOG 2>&1
+/usr/bin/scp -i $HOME/.ssh/id_dsa2 $OUTPUT $SERVER_LOC >> $LOG 2>&1
+
+d=`date`
+user=`whoami`
+echo "Performed driving.sh as $user on $d" >> $LOG
