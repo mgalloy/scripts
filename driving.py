0f734f0e78b589fc509f23df5725f18ca075ce39
Michael Galloy <mgalloy@gmail.com>
2015-04-24 19:15:43 -0600

    No spaces after commas in CSV!

--- driving.py
+++ driving.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             trip_start = row[0]
-            trip_mileage = row[11]
+            trip_mileage = row[10]
             dt = datetime.datetime.strptime(trip_start, ingest_date_format)
             day = dt.strftime(output_date_format)
             weeks[day] += float(trip_mileage)
