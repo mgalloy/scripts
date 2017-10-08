93ae540ff55eb1d5549a25cf40797f1c83610064
Michael Galloy <mgalloy@gmail.com>
2017-10-08 11:06:59 -0600

    Update ssh key

--- driving.sh
+++ driving.sh
@@ -8,7 +8,7 @@ LOG=$DATA_DIR/driving.log
 SERVER_LOC="idldev.com:~/data.idldev.com/"
 
 $BIN_DIR/driving.py $INPUT --output $OUTPUT >> $LOG 2>&1
-/usr/bin/scp -i $HOME/.ssh/id_dsa2 $OUTPUT $SERVER_LOC >> $LOG 2>&1
+/usr/bin/scp -i $HOME/.ssh/id_rsa2 $OUTPUT $SERVER_LOC >> $LOG 2>&1
 
 d=`date`
 user=`whoami`
