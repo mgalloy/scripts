8e4071910b7bc6e497534765edc4e038f9647f19
Michael Galloy <mgalloy@gmail.com>
2018-10-11 07:39:52 -0600

    Update to anaconda3

--- check_speed.sh
+++ check_speed.sh
@@ -2,7 +2,7 @@
 
 LOG=/Users/mgalloy/data/check_speed.py.log
 
-/Users/mgalloy/anaconda/bin/python /Users/mgalloy/bin/check_speed.py >> $LOG 2>&1
+/Users/mgalloy/anaconda3/bin/python /Users/mgalloy/bin/check_speed.py >> $LOG 2>&1
 /usr/bin/scp -i ~mgalloy/.ssh/id_rsa2 ~/data/speed.json idldev.com:~/data.idldev.com/ >> $LOG 2>&1
 
 d=`date`
